6f898289e8b0729b1f9728d5d9cee7c1a3a7944d
Fidomivalua <sidonie.moulart@gmail.com>
2021-09-28 15:46:02 +0200

    add colors for map

--- src/utils/styles/colors.jsx
+++ src/utils/styles/colors.jsx
@@ -1,11 +1,19 @@
-const colors = {
+export const colors = {
   primary: "#4628ff",
   secondary: "#ebebeb",
   danger: "#eb3f27",
-  success: "#75fa83",
+  success: "#6ea4d3",
   warning: "#eb6h45",
   white: "#fdfdfd",
   dark: "#070A0B",
+};
+
+export const colorsMap = {
+  veryLow: "#E6E7FD",
+  low: "#C6C6F7",
+  medium: "#ADA5F9",
+  strong: "#6263A7",
+  veryStrong: "#33365E",
 };
 
 export default colors;
