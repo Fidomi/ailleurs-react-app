035438163b358a95dcd729079f7e2da388117f68
Fidomivalua <sidonie.moulart@gmail.com>
2021-10-02 22:22:53 +0200

    Refine France map

---
+++ src/components/Departement/index.jsx
@@ -0,0 +1,13 @@
+function Departement(props) {
+  return (
+    <path
+      d={props.d}
+      data-name={props["dataName"]}
+      data-num={props["dataNum"]}
+      fill={props.fill}
+      stroke={props.stroke}
+    ></path>
+  );
+}
+
+export default Departement;
