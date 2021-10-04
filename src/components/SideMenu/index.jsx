549a2f0f07c2649535baed374e5ccffe3e80dd59
Fidomivalua <sidonie.moulart@gmail.com>
2021-10-04 18:54:55 +0200

    Add propTypes

--- src/components/SideMenu/index.jsx
+++ src/components/SideMenu/index.jsx
@@ -1,54 +1,33 @@
-import { Accordion, AccordionSection } from "../Accordion/index";
-import { StyledSideMenu } from "./style";
+import { Accordion, AccordionSection } from '../Accordion/index';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { StyledSideMenu } from './style';
+import { menuCategories as sideCategories } from '../../data/menuCategories';
 
-const sideCategories = [
-  {
-    id: 1,
-    name: "Cartes",
-    subcategories: [
-      {
-        id: 1,
-        name: `Vie Quotidienne`,
-        url: "../../pages/Maps/index",
-      },
-      {
-        id: 2,
-        name: `Santé et Sécurité`,
-        url: "../../pages/Maps/index",
-      },
-      {
-        id: 3,
-        name: `Environnement`,
-        url: "../../pages/Maps/index",
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: "Ma Destination",
-    url: "../../pages/MyDestiantion/index",
-  },
-];
-
-const SideMenu = (props) => {
-  return (
+const SideMenu = (props) => (
     <StyledSideMenu open={props.open} onClick={props.onClick}>
-      <AccordionSection>
-        {sideCategories.map((sideCategory) => (
-          <Accordion
-            subcategories={
-              sideCategory.subcategories ? sideCategory.subcategories : null
-            }
-            accordionTitle={sideCategory.name}
-            key={`id-${sideCategory.name}`}
-            onClik={() => console.log(sideCategory.subcategories)}
-          >
-            {sideCategory.name}
-          </Accordion>
-        ))}
-      </AccordionSection>
+        <AccordionSection>
+            {sideCategories.map((sideCategory) => (
+                <Accordion
+                    key={sideCategory.id}
+                    subcategories={
+                        sideCategory.subcategories
+                            ? sideCategory.subcategories
+                            : null
+                    }
+                    accordionTitle={sideCategory.name}
+                    onClik={() => console.log(sideCategory.subcategories)}
+                >
+                    {sideCategory.name}
+                </Accordion>
+            ))}
+        </AccordionSection>
     </StyledSideMenu>
-  );
+);
+
+SideMenu.propTypes = {
+    onClick: PropTypes.func,
+    open: PropTypes.bool.isRequired
 };
 
 export default SideMenu;
--- src/components/SideMenu/style.js
+++ src/components/SideMenu/style.js
@@ -1,13 +1,21 @@
-import styled from "styled-components";
-import { colors } from "../../utils/styles/colors";
+import { colors } from '../../utils/styles/colors';
+import device from '../../utils/styles/device';
+import styled from 'styled-components';
 
 export const StyledSideMenu = styled.div`
-  height: auto;
-  width: 100vw;
-  top: 3rem;
-  position: absolute;
-  left: 0;
-  display: ${({ open }) => (open ? "block" : "none")};
-  z-index: ${({ open }) => (open ? "1001" : "-100")};
-  background: ${colors.secondary};
+    height: auto;
+    width: 100vw;
+    top: 3rem;
+    position: absolute;
+    left: 0;
+    display: ${({ open }) => (open ? 'block' : 'none')};
+    z-index: ${({ open }) => (open ? '1001' : '-100')};
+    background: ${colors.secondary};
+
+    @media ${device.tablet} {
+        width: 30vw;
+        left: auto;
+        right: 0;
+        top: 4rem;
+    }
 `;
