ef3deafbf20d6c61a71f2cfdd676261b8b8c597d
Fidomivalua <sidonie.moulart@gmail.com>
2021-10-04 18:58:55 +0200

    Menu links description

---
+++ src/data/menuCategories.js
@@ -0,0 +1,28 @@
+export const menuCategories = [
+    {
+        id: 1,
+        name: 'Cartes',
+        subcategories: [
+            {
+                id: 1,
+                name: `Vie Quotidienne`,
+                url: '../../pages/Maps/index'
+            },
+            {
+                id: 2,
+                name: `Santé et Sécurité`,
+                url: '../../pages/Maps/index'
+            },
+            {
+                id: 3,
+                name: `Environnement`,
+                url: '../../pages/Maps/index'
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: 'Ma Destination',
+        url: '../../pages/MyDestiantion/index'
+    }
+];
