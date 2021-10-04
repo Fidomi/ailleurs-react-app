813d7c4b9842ed5b13e4fa2a2e3be047bd8cfc80
Fidomivalua <sidonie.moulart@gmail.com>
2021-10-04 12:23:45 +0200

    Add prettier config and correct errors

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import GlobalStyle from "./utils/styles/GlobalStyle";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Maps from "./pages/Maps";
 import MyDestination from "./pages/MyDestination";
-import Header from "./components/Header";
-import GlobalStyle from "./utils/styles/GlobalStyle";
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyle />
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/maps" component={Maps} />
-        <Route path="/mydestination" component={MyDestination} />
-        {/* <Route component={Error} /> */}
-        <Home />
-      </Switch>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <GlobalStyle />
+        <BrowserRouter>
+            <Header />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/maps" component={Maps} />
+                <Route path="/mydestination" component={MyDestination} />
+                {/* <Route component={Error} /> */}
+                <Home />
+            </Switch>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
