8b3c14c16e036b9a113f5170eb34cd3d3091d97d
Fidomivalua <sidonie.moulart@gmail.com>
2021-10-04 18:55:18 +0200

    Add propTypes

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,21 +1,31 @@
-import React from "react";
-import { StyledButton } from "./style";
+import PropTypes from 'prop-types';
+import React from 'react';
+import { StyledButton } from './style';
 
 export const Button = ({
-  children,
-  type,
-  onClick,
-  buttonStyle,
-  buttonSize,
-}) => {
-  return (
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    buttonSize
+}) => (
     <StyledButton
-      onClick={onClick}
-      type={type}
-      buttonSize={buttonSize}
-      buttonStyle={buttonStyle}
+        onClick={onClick}
+        type={type}
+        buttonSize={buttonSize}
+        buttonStyle={buttonStyle}
     >
-      {children}
+        {children}
     </StyledButton>
-  );
+);
+
+Button.propTypes = {
+    buttonSize: PropTypes.string.isRequired,
+    buttonStyle: PropTypes.string.isRequired,
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ]),
+    onClick: PropTypes.func,
+    type: PropTypes.string
 };
