ac1b086018663bf0aee3ace820f4ea72a69e91c0
Fidomivalua <sidonie.moulart@gmail.com>
2021-10-04 18:54:28 +0200

    State for accordion menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,48 +1,64 @@
-import { Link } from "react-router-dom";
-import { Button } from "../Button";
-import Logo from "../../assets/logo.png";
+/* eslint-disable max-lines-per-function */
+import BurgerMenu from '../BurgerMenu/index';
+import { Button } from '../Button';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/logo.png';
+// eslint-disable-next-line sort-imports
 import {
-  HeaderContainer,
-  NavLinks,
-  Title,
-  HeaderLogo,
-  TitleText,
-} from "./style";
-import BurgerMenu from "../BurgerMenu/index";
+    HeaderContainer,
+    HeaderLogo,
+    NavLinks,
+    Title,
+    TitleText
+} from './style';
+import { React, useState } from 'react';
+import SideMenu from '../SideMenu';
+import StyledLink from '../StyledLink/index';
 
 function Header() {
-  return (
-    <HeaderContainer>
-      <Link to="/">
-        <Title>
-          <HeaderLogo src={Logo} />
-          <TitleText>Ailleurs</TitleText>
-        </Title>
-      </Link>
-      <NavLinks>
-        <Link to="/maps">
-          <Button
-            type="button"
-            onClick={() => console.log("You clicked on me")}
-            buttonStyle="successOutline"
-            buttonSize="smallMobile"
-          >
-            Cartes
-          </Button>
-        </Link>
-        <Link to={"/myDestination"}>
-          <Button
-            type="button"
-            buttonStyle="successSolid"
-            buttonSize="smallMobile"
-          >
-            Ma Destination
-          </Button>
-        </Link>
-      </NavLinks>
-      <BurgerMenu />
-    </HeaderContainer>
-  );
+    const [isHover, setHover] = useState(false);
+    const [open, setOpen] = useState(false);
+    const handleOver = () => {
+        setHover(true);
+        setOpen(true);
+        console.log(isHover);
+    };
+
+    return (
+        <HeaderContainer>
+            <Link to="/">
+                <Title>
+                    <HeaderLogo src={Logo} />
+                    <TitleText>Ailleurs</TitleText>
+                </Title>
+            </Link>
+            <NavLinks>
+                <StyledLink to="/maps" onMouseOver={handleOver}>
+                    <Button
+                        type="button"
+                        buttonStyle="successOutline"
+                        buttonSize="smallMobile"
+                    >
+                        Cartes
+                    </Button>
+                </StyledLink>
+                {isHover && (
+                    <SideMenu open={open} OnMouseLeave={() => setOpen(false)} />
+                )}
+
+                <Link to={'/myDestination'}>
+                    <Button
+                        type="button"
+                        buttonStyle="successSolid"
+                        buttonSize="smallMobile"
+                    >
+                        Ma Destination
+                    </Button>
+                </Link>
+            </NavLinks>
+            <BurgerMenu open={open} handleOpen={setOpen} />
+        </HeaderContainer>
+    );
 }
 
 export default Header;
